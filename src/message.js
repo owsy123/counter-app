34fcf7d12637cedbc4cbe3b43407afe41b5187e6
owsy123 <owais.sohail3@gmail.com>
2020-06-12 13:18:06 +0500

    Update message.js

--- src/message.js
+++ src/message.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Message(props){
     return(
         <div>
-            <h1>Counter Application</h1>
+            <h1>Counter </h1>
             <h1>Counter <br/> {props.counter} </h1>
         </div>
     )
